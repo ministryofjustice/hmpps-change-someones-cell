4d9d726a982010d0c11f83bcb1cb6244a2be18f1
Jim Bailey <jim.bailey@digital.justice.gov.uk>
2024-01-15 15:48:17 +0000

    fix: Change REDIS_URL env var back to REDIS_HOST

--- server/config.ts
+++ server/config.ts
@@ -46,7 +46,7 @@ export default {
   https: production,
   staticResourceCacheDuration: '1h',
   redis: {
-    host: get('REDIS_URL', 'localhost', requiredInProduction),
+    host: get('REDIS_HOST', 'localhost', requiredInProduction),
     port: parseInt(process.env.REDIS_PORT, 10) || 6379,
     password: process.env.REDIS_AUTH_TOKEN,
     tls_enabled: get('REDIS_TLS_ENABLED', 'false'),
