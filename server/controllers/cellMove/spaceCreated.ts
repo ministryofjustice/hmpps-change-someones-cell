8535f76e445ff1b7d7687ad68c7f973d36b3593d
jimbali <40831617+jimbali@users.noreply.github.com>
2024-02-01 10:13:52 +0000

    feat: Space created page (#22)
    [MAP-681]

---
+++ server/controllers/cellMove/spaceCreated.ts
@@ -0,0 +1,26 @@
+import PrisonerDetailsService from '../../services/prisonerDetailsService'
+import { properCaseName } from '../../utils'
+
+type Params = {
+  prisonerDetailsService: PrisonerDetailsService
+}
+
+export default ({ prisonerDetailsService }: Params) =>
+  async (req, res) => {
+    const { offenderNo } = req.params
+    const { systemClientToken } = res.locals
+
+    try {
+      const { firstName, lastName } = await prisonerDetailsService.getDetails(systemClientToken, offenderNo)
+
+      return res.render('cellMove/spaceCreated.njk', {
+        title: `${properCaseName(firstName)} ${properCaseName(lastName)} has been moved`,
+        name: `${properCaseName(firstName)} ${properCaseName(lastName)}`,
+        prisonerSearchLink: '/prisoner-search',
+      })
+    } catch (error) {
+      res.locals.redirectUrl = `/prisoner/${offenderNo}/cell-move/search-for-cell`
+      res.locals.homeUrl = `/prisoner/${offenderNo}`
+      throw error
+    }
+  }
