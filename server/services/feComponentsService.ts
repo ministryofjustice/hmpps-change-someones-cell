2cc9993647e3f52f7c12501878fc091a7ee7426b
jimbali <40831617+jimbali@users.noreply.github.com>
2024-02-02 13:22:30 +0000

    fix: Get active caseload from frontend components API metadata if possible (#26)
    This makes sure that the caseload shown in the header matches the caseload
    that the app is aware of!
    
    [MAP-683]

--- server/services/feComponentsService.ts
+++ server/services/feComponentsService.ts
@@ -1,12 +1,9 @@
-import FeComponentsClient, { AvailableComponent, Component } from '../data/feComponentsClient'
+import FeComponentsClient, { AvailableComponent } from '../data/feComponentsClient'
 
 export default class FeComponentsService {
   constructor(private readonly feComponentsClient: FeComponentsClient) {}
 
-  async getComponents<T extends AvailableComponent[]>(
-    components: T,
-    token: string,
-  ): Promise<Record<T[number], Component>> {
+  async getComponents<T extends AvailableComponent[]>(components: T, token: string) {
     return this.feComponentsClient.getComponents(components, token)
   }
 }
