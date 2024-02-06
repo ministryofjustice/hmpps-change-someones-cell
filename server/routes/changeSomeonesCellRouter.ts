dbe5207fa84db3bc996ed53c1c68360df3a5c0ed
Jim Bailey <jim.bailey@digital.justice.gov.uk>
2024-02-06 14:51:24 +0000

    feat: Temporary move page
    [MAP-659]

--- server/routes/changeSomeonesCellRouter.ts
+++ server/routes/changeSomeonesCellRouter.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cellMoveHomepage from '../controllers/cellMove/cellMoveHomepage'
 import cellMovePrisonerSearch from '../controllers/cellMove/cellMovePrisonerSearch'
 import cellMoveViewResidentialLocation from '../controllers/cellMove/cellMoveViewResidentialLocation'
+import cellMoveTemporaryMove from '../controllers/cellMove/cellMoveTemporaryMove'
 
 const router = express.Router({ mergeParams: true })
 
@@ -9,6 +10,7 @@ const controller = services => {
   router.get('/', cellMoveHomepage)
   router.get('/prisoner-search', cellMovePrisonerSearch(services))
   router.get('/view-residential-location', cellMoveViewResidentialLocation(services))
+  router.get('/temporary-move', cellMoveTemporaryMove(services))
 
   return router
 }
