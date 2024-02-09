95560cc41eae9261c3caf99db019b6fe40233e96
Jim Bailey <jim.bailey@digital.justice.gov.uk>
2024-02-09 12:26:28 +0000

    feat: Back to start button
    [MAP-740]

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import type { Services } from '../services'
 import changeSomeonesCellRouter from './changeSomeonesCellRouter'
 import cellMoveRouter from './cellMoveRouter'
+import backToStart from '../controllers/backToStart'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(services: Services): Router {
@@ -11,6 +12,8 @@ export default function routes(services: Services): Router {
   router.use('/', changeSomeonesCellRouter(services))
 
   router.use('/prisoner/:offenderNo/cell-move', cellMoveRouter(services))
+
+  router.get('/back-to-start', backToStart())
 
   return router
 }
