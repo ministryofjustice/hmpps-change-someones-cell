cba108a3553a8aff0fd46c6590a450242ad2e9ba
Gurnank <gurnank.cheema@digital.justice.gov.uk>
2024-06-11 13:23:04 +0100

    MAP-1030 update integration tests

---
+++ integration_tests/mockApis/locationsInsidePrisonApi.ts
@@ -0,0 +1,128 @@
+import { stubFor } from './wiremock'
+
+export const stubHealth = (status = 200) =>
+  stubFor({
+    request: {
+      method: 'GET',
+      urlPath: '/locations-inside-prison-api/health/ping',
+    },
+    response: {
+      status,
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+    },
+  })
+
+export const stubGroups = (caseload, status = 200) => {
+  const json = [
+    {
+      name: '1',
+      key: '1',
+      children: [
+        {
+          name: 'A',
+          key: 'A',
+        },
+        {
+          name: 'B',
+          key: 'B',
+        },
+        {
+          name: 'C',
+          key: 'C',
+        },
+      ],
+    },
+    {
+      name: '2',
+      key: '2',
+      children: [
+        {
+          name: 'A',
+          key: 'A',
+        },
+        {
+          name: 'B',
+          key: 'B',
+        },
+        {
+          name: 'C',
+          key: 'C',
+        },
+      ],
+    },
+    {
+      name: '3',
+      key: '3',
+      children: [
+        {
+          name: 'A',
+          key: 'A',
+        },
+        {
+          name: 'B',
+          key: 'B',
+        },
+        {
+          name: 'C',
+          key: 'C',
+        },
+      ],
+    },
+  ]
+
+  const jsonSYI = [
+    {
+      name: 'block1',
+      key: 'block1',
+      children: [
+        {
+          name: 'A',
+          key: 'A',
+        },
+        {
+          name: 'B',
+          key: 'B',
+        },
+      ],
+    },
+    {
+      name: 'block2',
+      key: 'block2',
+      children: [
+        {
+          name: 'A',
+          key: 'A',
+        },
+        {
+          name: 'B',
+          key: 'B',
+        },
+        {
+          name: 'C',
+          key: 'C',
+        },
+      ],
+    },
+  ]
+
+  return stubFor({
+    request: {
+      method: 'GET',
+      url: `/locations-inside-prison-api/locations/prison/${caseload.id}/groups`,
+    },
+    response: {
+      status,
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      jsonBody: caseload.id === 'SYI' ? jsonSYI : json,
+    },
+  })
+}
+
+export default {
+  stubHealth,
+  stubGroups,
+}
