8954bd94586b2141fa15e117af80728edf2900fb
Gurnank <gurnank.cheema@digital.justice.gov.uk>
2024-07-13 17:25:14 +0100

    MAP-1403 update to govuk frontend 5.4.0

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+import * as govukFrontend from 'node_modules/govuk-frontend/dist/govuk/all'
+import * as mojFrontend from '@ministryofjustice/frontend'
+govukFrontend.initAll()
+mojFrontend.initAll()
