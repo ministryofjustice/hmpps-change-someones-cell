a48e3ca9c77ce7ff76a411cd980184ad989287e5
GCheema <gurnank.cheema@justice.gov.uk>
2025-12-04 07:56:56 +0000

    MAP-3066 enable implement allowed-scripts package

---
+++ .allowed-scripts.mjs
@@ -0,0 +1,10 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+   allowlist: {
+      "node_modules/@parcel/watcher@2.5.0": "ALLOW",
+      "node_modules/cypress@15.1.0": "ALLOW",
+      "node_modules/dtrace-provider@0.8.8": "ALLOW",
+      "node_modules/fsevents@2.3.3": "FORBID",
+   },
+})
