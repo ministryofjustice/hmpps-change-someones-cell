d577b6176c012ae24fd1bec97ba79fdaa21210b6
GCheema <gurnank.cheema@justice.gov.uk>
2026-03-19 10:49:38 +0000

    MAPB-348 fixing CVEs

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,9 +2,11 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
    allowlist: {
-      "node_modules/@parcel/watcher@2.5.0": "ALLOW",
-      "node_modules/cypress@15.1.0": "ALLOW",
-      "node_modules/dtrace-provider@0.8.8": "ALLOW",
-      "node_modules/fsevents@2.3.3": "FORBID",
+   "node_modules/@parcel/watcher@2.5.6": "<ALLO>",
+   "node_modules/cypress@15.1.0": "<REMOVED>",
+   "node_modules/cypress@15.12.0": "<ALLOW>",
+   "node_modules/dtrace-provider@0.8.8": "ALLOW",
+   "node_modules/fsevents@2.3.3": "FORBID",
+   "node_modules/unrs-resolver@1.11.1": "<ALLOW>"
    },
 })
