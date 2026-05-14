39414615f819ff92bff56242e44fd37650d0bbf0
Dan Benton <daniel.benton@justice.gov.uk>
2026-05-14 09:16:16 +0100

    Add node_modules/unrs-resolver@1.11.1 to allowed scripts

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -6,5 +6,6 @@ export default configureAllowedScripts({
     'node_modules/cypress@15.14.2': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'FORBID',
+    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
 })
